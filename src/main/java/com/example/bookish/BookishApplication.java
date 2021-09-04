26c041831be4df3d1a5ac0fb90d4390ddc86f9b0
rkgoudra <yash.cool995@gmail.com>
2021-09-04 20:39:59 +0530

    initializing bookish project with STS changes

---
+++ src/main/java/com/example/bookish/BookishApplication.java
@@ -0,0 +1,13 @@
+package com.example.bookish;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookishApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookishApplication.class, args);
+	}
+
+}
